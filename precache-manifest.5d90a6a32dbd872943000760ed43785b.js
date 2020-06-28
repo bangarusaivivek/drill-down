abdeacfc9a8bdc642084518da680d3cab80aec8a
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-06-28 17:07:25 +0530

    Updates

---
+++ precache-manifest.5d90a6a32dbd872943000760ed43785b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6a1742474d4240504ecb263263556e",
+    "url": "/drill-down/index.html"
+  },
+  {
+    "revision": "4efc94240a1e3c05774e",
+    "url": "/drill-down/static/css/main.a7ceebfa.chunk.css"
+  },
+  {
+    "revision": "7eac5ddacc729ae5f9ce",
+    "url": "/drill-down/static/js/2.851e0a83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drill-down/static/js/2.851e0a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efc94240a1e3c05774e",
+    "url": "/drill-down/static/js/main.99910c0e.chunk.js"
+  },
+  {
+    "revision": "afc0d53973aa903cb184",
+    "url": "/drill-down/static/js/runtime-main.b10c56f1.js"
+  }
+]);
